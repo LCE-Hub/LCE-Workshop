8e5c92c0d0a092377b9b0037784b6601f60feb08
/home/neo <158327205+neoapps-dev@users.noreply.github.com>
2026-07-12 10:12:12 +0300

    fix

--- .00plugins/multiplayer-button/main.js
+++ .00plugins/multiplayer-button/main.js
@@ -2,6 +2,6 @@ api.actions.register("home-menu", {
   id: "multiplayer",
   label: "Multiplayer",
   onClick: function () {
-    api.views.navigate("lcelive");
+    api.views.navigate("lceonline");
   },
 });
